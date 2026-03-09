b373167f481fd3c3a82712a75534eaaf103d64ce
H-Stranger <3547845923@qq.com>
2026-03-09 20:53:09 +0800

    冲突解决

---
+++ src/test/java/com/itheima/JavawebTestApplicationTests.java
@@ -0,0 +1,18 @@
+package com.itheima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavawebTestApplicationTests {
+
+    @Test
+    public void testHello(){
+        System.out.println("hello world");
+    }
+    @Test
+    public void testHello2(){
+        System.out.println("hello world3");
+    }
+
+}
